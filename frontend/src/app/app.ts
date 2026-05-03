de64b9a5141e049bf762f84ed95c142dc4792e17
HRSH455 <bommiharsha7@gmail.com>
2026-05-03 14:05:43 +0530

    feat: Health , LandingPage while render warms up

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,38 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class App {
+export class App implements OnInit {
   title = 'API Rate Limiter';
+  isBackendReady = false;
+  waitSeconds = 0;
+  private timer: any;
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.startTimer();
+    this.pollBackend();
+  }
+
+  private startTimer() {
+    this.timer = setInterval(() => {
+      this.waitSeconds++;
+      this.cdr.markForCheck();
+    }, 1000);
+  }
+
+  private pollBackend() {
+    const backendUrl = 'https://apiratelimiter-id8y.onrender.com'; // 🔴 replace with your Render URL
+    const poll = () => {
+      this.http.get(`${backendUrl}/actuator/health`).subscribe({
+        next: () => {
+          clearInterval(this.timer);
+          this.isBackendReady = true;
+          this.cdr.markForCheck();
+        },
+        error: () => setTimeout(poll, 5000)
+      });
+    };
+    poll();
+  }
 }
